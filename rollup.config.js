a19a548556cb246b60a9a8763f6cbcf4142d44d5
Jason Miller <developit@users.noreply.github.com>
2017-02-26 11:53:38 -0500

    Update rollup.config.js

--- rollup.config.js
+++ rollup.config.js
@@ -1,14 +1,10 @@
 import buble from 'rollup-plugin-buble';
 
-let { FORMAT } = process.env;
-
 export default {
 	useStrict: false,
 	sourceMap: true,
 	entry: 'src/index.js',
 	plugins: [
-		buble(),
-		FORMAT==='cjs',
-		FORMAT==='umd'
+		buble()
 	]
 };
